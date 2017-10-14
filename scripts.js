be6fad90269a8ed44cd24addeb6a2f4240a09d14
Ammelie <ammelie.neth@student.kyh.se>
2017-10-15 00:34:40 +0200

    added content and finished up the design

--- scripts.js
+++ scripts.js
@@ -1,10 +1,62 @@
-window.onload = () => {
-	let portrait = document.getElementById('portrait2')
+$(function() {
+	$('#portrait2').on('mouseover', smile)
+	$('.about-link').on('click', smile)
+	$('.nav-link').on('click', scrollDown)
+	$('.go-back').on('click', backToTop)
+});
 
-	portrait.onmouseover = () => {
-		portrait.classList.toggle('smile')
-		window.setTimeout(() => {
-			portrait.classList.toggle('smile')
-		}, 1500)
-	}
+
+
+/////////////////////////////////
+// Makes portrait smile creepily. Happy Halloween!
+/////////////////////////////////
+function smile() {
+	$('#portrait2').toggleClass('smile')
+	setTimeout(() => {
+		$('#portrait2').toggleClass('smile')
+	}, 1500)
+}
+
+
+
+/////////////////////////////////
+// Scroll down
+/////////////////////////////////
+function scrollDown(e) {
+	e.preventDefault()
+
+	displayBackButton()
+
+	// Every section is assigned an id that is also in the link
+	let link = $(this).attr('href')
+	let section = document.getElementById(link.split("#")[1])
+
+	section.scrollIntoView({
+		behavior: 'smooth',
+		block: 'start'
+	})
+}
+
+
+
+/////////////////////////////////
+// Scroll back up and hide back button
+/////////////////////////////////
+function backToTop() {
+	document.getElementById('intro')
+	intro.scrollIntoView({
+		behavior: 'smooth',
+		block: 'start'
+	})
+
+	$('.go-back').removeClass('make-visible')
+}
+
+
+
+/////////////////////////////////
+// Make back button visible
+/////////////////////////////////
+function displayBackButton() {
+	$('.go-back').addClass('make-visible')
 }
